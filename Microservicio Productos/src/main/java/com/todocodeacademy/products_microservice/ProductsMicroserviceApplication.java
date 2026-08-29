983d6fce78957760967702796c223377277a1c57
Tomaselbert2000 <tomas@debian.tomas>
2026-08-29 19:29:37 -0300

    feat: agrega base para Microservicio de Productos

---
+++ Microservicio Productos/src/main/java/com/todocodeacademy/products_microservice/ProductsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.todocodeacademy.products_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsMicroserviceApplication {
+
+	static void main(String[] args) {
+
+		SpringApplication.run(ProductsMicroserviceApplication.class, args);
+	}
+}
