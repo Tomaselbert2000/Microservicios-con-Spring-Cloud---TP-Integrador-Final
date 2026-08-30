d6e0664ad6cceaefeadf6c7335d8fe602f6f399d
Tomaselbert2000 <tomas@debian.tomas>
2026-08-30 15:39:39 -0300

    feat: agrega dependencia de mapper y logica de CRUD inicial en ProductServiceImpl

--- Microservicio Productos/src/main/java/com/todocodeacademy/products_microservice/service/ProductServiceImpl.java
+++ Microservicio Productos/src/main/java/com/todocodeacademy/products_microservice/service/ProductServiceImpl.java
@@ -1,36 +1,63 @@
 package com.todocodeacademy.products_microservice.service;
 
 import com.todocodeacademy.products_microservice.dto.ProductDTO;
+import com.todocodeacademy.products_microservice.mapper.interfaces.ProductMapper;
+import com.todocodeacademy.products_microservice.model.Product;
 import com.todocodeacademy.products_microservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository repository;
-
+    private final ProductMapper mapper;
 
     @Override
+    @Transactional
     public void createProduct(ProductDTO dto) {
 
+        Product product = mapper.mapDTOtoEntity(dto);
+
+        repository.save(product);
     }
 
     @Override
+    @Transactional
     public void deleteProduct(String productName) {
 
+        Product product = repository.findProductByName(productName);
+
+        if (product != null) {
+
+            repository.delete(product);
+        }
     }
 
     @Override
     public ProductDTO getProductByName(String productName) {
+
+        Product product = repository.findProductByName(productName);
+
+        if (product != null) {
+
+            return mapper.mapEntityToDTO(product);
+        }
+
         return null;
     }
 
     @Override
     public List<ProductDTO> getAllProducts() {
+
+        List<Product> products = repository.findAll();
+
+        if (!products.isEmpty()) return products.stream().map(mapper::mapEntityToDTO).collect(Collectors.toList());
 
         return List.of();
     }
