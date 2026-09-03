ff51b0af970ebcbfaf581d783b66baf7731a328b
Tomaselbert2000 <tomas@debian.tomas>
2026-09-03 13:09:59 -0300

    feat(cart-microservice): implementa REST controller para carritos de compra

---
+++ cart-microservice/src/main/java/com/todocodeacademy/cart_service/controller/CartController.java
@@ -0,0 +1,47 @@
+package com.todocodeacademy.cart_service.controller;
+
+import com.todocodeacademy.cart_service.dto.CartDTO;
+import com.todocodeacademy.cart_service.service.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.todocodeacademy.cart_service.constants.StringResource.ControllerConstants.CART_CREATED_SUCCESSFULLY;
+import static com.todocodeacademy.cart_service.constants.StringResource.ControllerConstants.CART_DELETED_SUCCESSFULLY;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/carts")
+public class CartController {
+
+    private final CartService service;
+
+    @GetMapping("/all")
+    public List<CartDTO> getAllCarts() {
+
+        return service.getAllCarts();
+    }
+
+    @GetMapping("/{cartID}")
+    public CartDTO getCartInfoByID(@PathVariable Long cartID) {
+
+        return service.getCartInfoByID(cartID);
+    }
+
+    @PostMapping("/create")
+    public String createCart(@RequestBody CartDTO dto) {
+
+        service.saveCart(dto);
+
+        return CART_CREATED_SUCCESSFULLY;
+    }
+
+    @DeleteMapping("/delete/{cartID}")
+    public String deleteCart(@PathVariable Long cartID) {
+
+        service.deleteCart(cartID);
+
+        return CART_DELETED_SUCCESSFULLY;
+    }
+}
