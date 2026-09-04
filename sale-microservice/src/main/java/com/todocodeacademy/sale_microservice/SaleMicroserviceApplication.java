844af2357a790f140e79dcf30961a071ab43485d
Tomaselbert2000 <tomas@debian.tomas>
2026-09-04 14:33:56 -0300

    fix(sale-microservice): corrige annotation faltante

--- sale-microservice/src/main/java/com/todocodeacademy/sale_microservice/SaleMicroserviceApplication.java
+++ sale-microservice/src/main/java/com/todocodeacademy/sale_microservice/SaleMicroserviceApplication.java
@@ -2,10 +2,12 @@ package com.todocodeacademy.sale_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class SaleMicroserviceApplication {
 
 	static void main(String[] args) {
